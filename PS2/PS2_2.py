d73165b5b76154f0eb67a6310c9018e822253ae6
V S Ritanya <146947833+ritanya-vs@users.noreply.github.com>
2026-01-28 11:46:31 +0530

    Add files via upload

---
+++ PS2/PS2_2.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("ps2_2_data.csv",header = None)
+
+n,p = data.shape
+
+def dissimilarity(i,j):
+    m = (data.iloc[i] == data.iloc[j]).sum()
+    return (p - m)/p
+
+
+print("DISSIMILARITY MATRIX")
+for i in range(n):
+    for j in range(n):
+        res = dissimilarity(i, j)
+        print(res," ")
+    print()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+def dissimilarity(i, j):
+    m = (data.iloc[i] == data.iloc[j]).sum()
+    return (p - m) / p
+
+print("Dissimilarity Matrix:\n")
+
+for i in range(n):
+    for j in range(n):
+        d = dissimilarity(i, j)
+        print(f"{d:.2f}", end="  ")
+    print()
+
